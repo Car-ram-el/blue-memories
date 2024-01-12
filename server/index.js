edd7df526a499f838e72ca589c61a78981d2186f
Car-ram-el <122819426+Car-ram-el@users.noreply.github.com>
2024-01-12 14:41:50 +0530

    Update index.js
    add get / route

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json({limit:"30mb",extended:true})); // for images of larger 
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true})); // json like xp with url encoded format
 app.use(cors());
 app.use('/posts',postRoutes);
-
+app.get('/',(req,res)=>res.send("hello memories."));
 // database
 const PORT=process.env.PORT || 5000;
 mongoose.connect(process.env.CONNECTION_URL)
